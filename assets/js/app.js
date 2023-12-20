a325006c059190a80a8b2cc2eccac6413c4292a8
Cat-celestial <kirillkoten@gmail.com>
2023-12-21 00:22:02 +0200

    lesson 2 js B1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,15 @@
 let inp = document.querySelector(".inp");
 inp.focus();
 
-
 let letters = {
-    "a": "Alfa", "b": "Bravo", "c": "Charlie", "d": "Delta", "e": "Echo",
-    "f": "Foxtrot", "g": "Golf", "h": "Hotel", "i": "India", "j": "Juliett",
-    "k": "Kilo", "l": "Lima", "m": "Mike", "n": "November", "o": "Oscar",
-    "p": "Papa", "q": "Quebec", "r": "Romeo", "s": "Sierra", "t": "Tango",
-    "u": "Unoform", "v": "Victor", "w": "Whiskey", "x": "X - ray", "y": "Yankee",
-    "z": "Zulu"
+    a: "Alfa", b: "Bravo", c: "Charlie", d: "Delta", e: "Echo",
+    f: "Foxtrot", g: "Golf", h: "Hotel", i: "India", j: "Juliett",
+    k: "Kilo", l: "Lima", m: "Mike", n: "November", o: "Oscar",
+    p: "Papa", q: "Quebec", r: "Romeo", s: "Sierra", t: "Tango",
+    u: "Unoform", v: "Victor", w: "Whiskey", x: "X - ray", y: "Yankee",
+    z: "Zulu"
 }
+
 
 let numbers = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Niner"];
 
@@ -19,16 +19,15 @@ function decodeFunc() {
     let decodeLab = document.querySelector(".decoding");
     let inpValue = inp.value;
     decodeLab.textContent = "Разшифровка: ";
-    
-    
-    for (let item of inpValue) {
-        console.log(numbers[item]);
-        if (typeof(item) == "number") {
-            let numItem = parseInt(item);
-            decodeLab.textContent += numbers[numItem] + " ";
 
-        } else if (typeof(item) == "string") {
-            decodeLab.textContent += letters[item] + " ";
+    for (let item = 0; item <= inpValue.length - 1; item++) {
+        let inNum = !isNaN(inpValue[item]);
+
+        if (inNum) {
+            decodeLab.textContent += numbers[inpValue[item]] + " ";
+        } else if (inNum == false){
+            
+            decodeLab.textContent += letters[inpValue[item]] + " ";
         }
     }
 }
