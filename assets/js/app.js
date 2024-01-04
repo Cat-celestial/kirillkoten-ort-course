35aab28ebe40c7b5c8f5c1049ad9ff5fc150bac0
Cat-celestial <kirillkoten@gmail.com>
2024-01-04 21:48:10 +0200

    урок 5

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,45 @@
-let creditSum = +prompt("Введите суму кредита");
-// let creditSumInUse = creditSum;
-let annualRate = +prompt("Введите годовой процент");
-let monthLoanTerm = +prompt("Введите срок кредита в месяцах");
-let monthPaym = (creditSum / monthLoanTerm).toFixed(2);
+let inp = document.querySelector("#mainInp");
+inp.focus();
+let validLabel = document.querySelector("#validCardLab");
+let nameBankLab = document.querySelector("#bankNameLab");
 
-let overPaym = 0;
+function mainFunc() {
+    validLabel.innerHTML = "Карта валидная?";
+    nameBankLab.innerHTML = "Карта банка: - ";
+    let card = inp.value.split("").map(item => +item).reverse();
+    let sum = 0;
 
-for (let i = 0; i < monthLoanTerm; i++) {
-    let paymLeft = creditSum - i * monthPaym;
-    // creditSumInUse = paymLeft;
+    for (let i = 0; i < card.length; i++) {
+        if (i % 2 == 0) {
+            sum += card[i];
+        } else {
+            let num = card[i] * 2;
+            sum = sum + (num > 9 ? num - 9 : num);
+        }
+    }
 
-    let ratePaym = paymLeft * (annualRate / 12 / 100);
-    overPaym += ratePaym;
-    console.log(`Задолженность по кредиту: ${(paymLeft).toFixed(2)}, Погашение кредита: ${monthPaym}, Проценты по кредиту: ${(ratePaym).toFixed(2)}, Общая выплата в месяц: ${(paymLeft + ratePaym).toFixed(2)}`);
+    if (sum % 10 === 0) {
+        validLabel.textContent = "Карта является валидной";
+    } else {
+        validLabel.textContent = "Карта не является валидной";
+    }
+
+    card = card.reverse();
+    // console.log(card, "масив с обычной картой")
+    let visaCardLengs = [13, 16, 19];
+    let first4Nums = parseInt(card.join("").slice(0, 4));
+    let first2Nums = parseInt(card.join("").slice(0, 2));
+    // console.log(first4Nums, "первые 4 цифры");
+    // console.log("Первая цифра", card[0], typeof(card[0]));
+
+    if ((card.length == 16 || card.length == 13 || card.length == 19) && card[0] == 4) {
+        nameBankLab.innerHTML = `Карта банка: Visa`;
+    } else if (card.length == 16 && ((first4Nums >= 2221 && first4Nums <= 2720) || (first2Nums >= 51 && first2Nums <=55))) {
+        nameBankLab.innerHTML = `Карта банка: Mastercard`;
+    } else if (card.length == 15 && (first2Nums == 34 || first2Nums == 37)){
+        nameBankLab.innerHTML = `Карта банка: AmericanExpress`;
+    } else {
+        nameBankLab.innerHTML = `Карта банка: неизвестная`;
+    }
 }
 
-console.log("Переплата по кредиту: ", (overPaym).toFixed(2));
