5d91034b16fa0f83c9332199dc70ef8675dfe1c2
Cat-celestial <kirillkoten@gmail.com>
2024-01-15 12:13:06 +0200

    урок 8 делаю стилизацию первой части КУРСЫ валют

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,31 @@
-let inp = document.querySelector("#mainInp");
-inp.focus();
-let validLabel = document.querySelector("#validCardLab");
-let nameBankLab = document.querySelector("#bankNameLab");
+const urlCounts = await fetch('https://restcountries.com/v3.1/all');
+const urlCurr = await fetch("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json");
+const countriesObj = await urlCounts.json();
+const currensObj = await urlCurr.json();
 
-function mainFunc() {
-    validLabel.innerHTML = "Карта валидная?";
-    nameBankLab.innerHTML = "Карта банка: - ";
-    let card = inp.value.split("").map(item => +item).reverse();
-    let sum = 0;
+document.querySelector(".titleLab").textContent += currensObj[0].exchangedate;
 
-    for (let i = 0; i < card.length; i++) {
-        if (i % 2 == 0) {
-            sum += card[i];
-        } else {
-            let num = card[i] * 2;
-            sum = sum + (num > 9 ? num - 9 : num);
-        }
-    }
+console.log(countriesObj);
+console.log(currensObj);
 
-    if (sum % 10 === 0) {
-        validLabel.textContent = "Карта является валидной";
-    } else {
-        validLabel.textContent = "Карта не является валидной";
-    }
+for (let curr in currensObj) {
+    let mBoxTag = document.querySelector(".mainBox");
+    let newCurrBox = document.createElement("div");
 
-    card = card.reverse();
-    // console.log(card, "масив с обычной картой")
-    let visaCardLengs = [13, 16, 19];
-    let first4Nums = parseInt(card.join("").slice(0, 4));
-    let first2Nums = parseInt(card.join("").slice(0, 2));
-    // console.log(first4Nums, "первые 4 цифры");
-    // console.log("Первая цифра", card[0], typeof(card[0]));
+    newCurrBox.innerHTML = `
+        <div class="currBox">
+            <h2 class="textSimp currName">${currensObj[curr].txt} (${currensObj[curr].cc})</h2>
+            <h3 class="textSimp rateLab" >Курс: </h3>
+            <h3 class="textSimp currLab"> ${currensObj[curr].rate.toFixed(2)}грн</h3>
+        </div>
+    `;
 
-    if ((card.length == 16 || card.length == 13 || card.length == 19) && card[0] == 4) {
-        nameBankLab.innerHTML = `Карта банка: Visa`;
-    } else if (card.length == 16 && ((first4Nums >= 2221 && first4Nums <= 2720) || (first2Nums >= 51 && first2Nums <=55))) {
-        nameBankLab.innerHTML = `Карта банка: Mastercard`;
-    } else if (card.length == 15 && (first2Nums == 34 || first2Nums == 37)){
-        nameBankLab.innerHTML = `Карта банка: AmericanExpress`;
-    } else {
-        nameBankLab.innerHTML = `Карта банка: неизвестная`;
-    }
+    mBoxTag.appendChild(newCurrBox);
 }
 
+let allCurrLabs = document.querySelectorAll(".currName");
+
+for (let item in allCurrLabs) {
+    
+    
+}
