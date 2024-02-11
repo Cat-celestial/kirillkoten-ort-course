422628d4a5386ba2bf8e9759ba165c663f022340
Cat-celestial <kirillkoten@gmail.com>
2024-02-11 23:51:48 +0200

    урок 14 первая задача: перевод цен только в гривны

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,54 +1,63 @@
-let data = `Pellentesque ex eros, +380665433321 porttitor eu bibendum ac, aliquam tincidunt urna. 
-Mauris tristique lobortis orci, nec varius magna convallis interdum. Etiam 5363 4567 8765 3454 
-pharetra tempor ex, vel eleifend (067) 678 44 21 odio lacinia (0562) 35-30-38 eget. Morbi maximus 
-libero vitae aliquet facilisis. Vivamus 5674-2346-8945-0012 vitae quam nisi. Quisque 12/45 quis 
-venenatis 5192722517688913 lacus. Sed ac lorem (050)567-45-33 nec leo pharetra 4556796335044346 
-dapibus sed eu +38067432112 ex. In hac 4913-8185-2881-4543  habitasse platea dictumst. In dignissim 
-5461158320267908 suscipit rutrum. Ut 4916 8494 1754 2904 luctus  sapien in risus 56th street auctor, 
-ac placerat 067-678-44-21  quam malesuada. Pellentesque (056) 7783322 bibendum justo  5363 4567 87653 3455 
-tempus purus convallis, a viverra nunc ullamcorper. Nulla 5213 9203 2475 5355 eget lectus gravida, porta 
-eros vitae, semper erat +39-926-1234567.  Aenean volutpat vehicula dui ut pharetra.`;
+import { createApp, ref } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
-let checker1 = /[0-9]{16}|[0-9]{4} [0-9]{4} [0-9]{4} [0-9]{4}|[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}/g;
-let allCardsBegin = data.match(checker1);
-let allCards = [];
-let allGappedCards = []
 
-for (let card of allCardsBegin) {
-    let updatedCard = "";
+let fetchedSite = await fetch("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json");
+let jsonFile = await fetchedSite.json();
 
-    for (let el of card) {
-        if (Number.isInteger(parseInt(el))) {
-            updatedCard += el;
+let app = {
+    data: jsonFile,
+    loadData() {
+        this.render();
+    },
+    render() {
+        console.log(this.data);
+
+        let allProdBoxes = document.querySelectorAll('#phones_ul li');
+        let allCurrSpans = document.getElementsByClassName("badge bg-primary badge-pill");
+        // console.log(allCurrSpans);
+        // console.log(allProdBoxes);
+
+        for (let box in allCurrSpans) {
+            // console.log(box, typeof(parseInt(box)));
+            if ("012345678".includes(box)) {
+                console.log(allCurrSpans[box].outerText.slice(0, 3));
+
+                for (let curr of this.data) {
+                    if (curr.cc == allCurrSpans[box].outerText.slice(0, 3)) {
+                        let newTag = document.createElement("span");
+
+                        newTag.innerHTML = `
+                            <span class="badge bg-primary">HRN ${(parseInt(allCurrSpans[box].outerText.slice(-3)) * curr.rate).toFixed(2)}</span>
+                        `;
+
+                        newTag.style.width = "100px";
+                        allProdBoxes[box].appendChild(newTag);
+                    }
+
+                }
+            }
         }
     }
-
-    allCards.push(updatedCard);
-    allGappedCards.push(updatedCard.replace(/(\d{4})/g, '$1 ').trim());
 }
 
-console.log(allCards);
-console.log(allGappedCards);
-
-console.log("Валидные карты: ");
-
-for (let cd in allCards) {
-    let card = allCards[cd].split("").map(item => +item).reverse();
-    // console.log(card, typeof(card));
-    let sum = 0;
-
-    for (let i = 0; i < card.length; i++) {
-        if (i % 2 == 0) {
-            sum += card[i];
-        } else {
-            let num = card[i] * 2;
-            sum = sum + (num > 9 ? num - 9 : num);
-        }
-    }
-    if (sum % 10 === 0) {
-        console.log(allGappedCards[cd]);
-    }
+app.loadData();
 
 
-}
 
+
+
+
+// let url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
+// let fetchedSite = await fetch(url);
+// let jsonFile = await fetchedSite.json();
+
+// createApp({
+//     setup() {
+
+
+//         return {
+
+//         }
+//     }
+
+// }).mount(".container");
