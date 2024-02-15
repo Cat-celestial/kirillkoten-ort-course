6015f2add75779ce3f4fb8bf62a7c79f6ae3a090
Cat-celestial <kirillkoten@gmail.com>
2024-02-16 00:47:21 +0200

    ааааааааа урок 14 доделаный хорошо

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,69 +3,50 @@ import { createApp, ref } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js"
 
 let fetchedSite = await fetch("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json");
 let jsonFile = await fetchedSite.json();
-jsonFile[61] = { "cc": "HRN", "exchangedate": jsonFile[0].exchangedate, "rate": 1, "txt": "Українська гривня" }
+jsonFile[61] = { "cc": "UAH", "exchangedate": jsonFile[0].exchangedate, "rate": 1, "txt": "Українська гривня" }
 
 let selectTag = document.querySelector("#selectTag");
+let toExchLabs = document.getElementsByClassName("toUAHLab");
+// console.log(toExchLabs);
 
 let app = {
     data: jsonFile,
-    toCurr: "HRN",
+    toCurr: "UAH",
     loadData() {
         this.clearToCurrLubs();
         this.render();
     },
     render() {
-        console.log(this.data);
+        // console.log(this.data);
 
         let allProdBoxes = document.querySelectorAll('#phones_ul li');
         let allCurrSpans = document.getElementsByClassName("badge bg-primary badge-pill");
-        let toExchLabs = document.querySelectorAll(".toHrnLab")
         // console.log(allCurrSpans);
         // console.log(allProdBoxes);
 
         for (let box in allCurrSpans) {
+            let intBox = parseInt(box);
+
             if ("012345678".includes(box)) {
-                // console.log(allCurrSpans[box].outerText.slice(0, 3));
+                this.data.filter(item => item.cc.includes(allCurrSpans[intBox].outerText.slice(0, 3))).map((curr, index) => {
+                    let priceMatch = allCurrSpans[intBox].outerText.match(/\d+/g);
+                    let needCurRateUAH = parseInt(priceMatch[priceMatch.length - 1], 10) * curr.rate;
+                    let rightCurr = "UAH";
 
-                for (let curr of this.data) {
-                    let newCurrName = document.createElement("option");
-                    newCurrName.innerHTML = `
-                        <option id="${curr.сс}">${curr.txt} (${curr.cc})</option>
-                    `;
-
-                    selectTag.appendChild(newCurrName);
-
-                    if (curr.cc == allCurrSpans[box].outerText.slice(0, 3)) {
-                        let newTag = document.createElement("span");
-
-                        if (this.toCurr != "HRN") {
-                            let priceMatch = allCurrSpans[box].outerText.match(/\d+/g);
-                            let needCurRateHRN;
-                            let rightCurr;
-
-                            for (let needCur of this.data) {
-                                if (needCur.cc == this.toCurr) {
-                                    needCurRateHRN = parseInt(priceMatch[priceMatch.length - 1], 10) * curr.rate / needCur.rate;
-                                    rightCurr = needCur.cc;
-                                }
+                    if (this.toCurr != "UAH") {
+                        for (let needCur of this.data) {
+                            if (needCur.cc == this.toCurr) {
+                                needCurRateUAH = parseInt(priceMatch[priceMatch.length - 1], 10) * curr.rate / needCur.rate;
+                                rightCurr = needCur.cc;
                             }
-
-                            newTag.innerHTML = `
-                                <span class="badge bg-primary toHrnLab">${rightCurr} ${(needCurRateHRN).toFixed(2)}</span>
-                            `;
-
-                        } else if (this.toCurr == "HRN") {
-                            let priceMatch = allCurrSpans[box].outerText.match(/\d+/g);
-                            newTag.innerHTML = `
-                                <span class="badge bg-primary toHrnLab">HRN ${(parseInt(priceMatch[priceMatch.length - 1], 10) * curr.rate).toFixed(2)}</span>
-                            `;
                         }
-
-                        newTag.style.width = "100px";
-                        allProdBoxes[box].appendChild(newTag);
                     }
 
-                }
+                    toExchLabs[intBox].outerHTML = `
+                        <span class="badge bg-primary toUAHLab">${rightCurr} ${(needCurRateUAH).toFixed(2)}</span>
+                    `;
+                });
+
             }
         }
 
@@ -77,20 +58,79 @@ let app = {
     },
 
     clearToCurrLubs() {
-        let elementsToRemove = document.querySelectorAll('.toHrnLab');
+        let elementsToRemove = document.querySelectorAll('.toUAHLab');
 
         elementsToRemove.forEach(function (element) {
             element.innerHTML = "";
         });
+    },
+    appendSelect() {
+        let allCurArray = [];
+
+        for (let curr of this.data) {
+            allCurArray.push(`${curr.txt} (${curr.cc})`);
+        }
+        allCurArray.sort(function (a, b) {
+            return a.localeCompare(b, 'uk-UA');
+        });
+
+        for (let curr of allCurArray) {
+            let newCurrName = document.createElement("option");
+            newCurrName.innerHTML = `
+                <option id="${curr.match(/\(([^)]+)\)/)[1].trim()}">${curr}</option>
+            `;
+            selectTag.appendChild(newCurrName);
+
+        }
     },
 
     sortOptions() {
     }
 }
 app.loadData();
-// app.sortOptions();
+app.appendSelect();
 
 selectTag.addEventListener("change", () => {
     app.changeToWhatCurr(selectTag.value);
 
 })
+
+
+
+
+
+
+// this.data.filter(item => item.cc.includes(allCurrSpans[box].outerText.slice(0, 3))).map((curr, index) => {
+
+//     // let newTag = document.createElement("span");
+
+//     if (this.toCurr != "UAH") {
+//         let priceMatch = allCurrSpans[box].outerText.match(/\d+/g);
+//         let needCurRateUAH;
+//         let rightCurr;
+
+//         for (let needCur of this.data) {
+//             if (needCur.cc == this.toCurr) {
+//                 needCurRateUAH = parseInt(priceMatch[priceMatch.length - 1], 10) * curr.rate / needCur.rate;
+//                 rightCurr = needCur.cc;
+//             }
+//         }
+
+//         this.toExchLabs[index].innerHTML = `
+//             <span class="badge bg-primary toUAHLab">${rightCurr} ${(needCurRateUAH).toFixed(2)}</span>
+//         `;
+
+//         // newTag.style.width = "100px";
+//         // allProdBoxes[box].appendChild(newTag);
+
+//     } else if (this.toCurr == "UAH") {
+//         let priceMatch = allCurrSpans[box].outerText.match(/\d+/g);
+//         newTag.innerHTML = `
+//             <span class="badge bg-primary toUAHLab">UAH ${(parseInt(priceMatch[priceMatch.length - 1], 10) * curr.rate).toFixed(2)}</span>
+//         `;
+//         newTag.style.width = "100px";
+//         allProdBoxes[box].appendChild(newTag);
+
+//     }
+
+// });
